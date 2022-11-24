1a0a1e4ac281603a161f0de6a16463634d437500
sonjibony <sonjibony@gmail.com>
2022-11-24 14:28:44 +0600

    basic mongodb set up done

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nhngvm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run (){
+try{
+//collections
+const categoryCollection = client.db('bigDeal').collection('categories');
+}
+finally{
 
+}
+}
+run().catch(console.log)
 
 
 app.get("/", async (req, res) => {
