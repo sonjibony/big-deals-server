7053be9da38641092edb82fe071727af5c698bad
sonjibony <sonjibony@gmail.com>
2022-11-24 14:23:02 +0600

    env variables added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,14 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3nhngvm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get("/", async (req, res) => {
   res.send("big deal server is running");
